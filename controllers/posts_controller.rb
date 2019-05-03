f81ce17bed9cb7fda092c5f10e366a4a5b637be8
Tech-A24 <tech-a24@Tech-A24s-MacBook-Air.local>
2019-05-03 09:30:29 +0100

    added links for images

--- controllers/posts_controller.rb
+++ controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostController < Sinatra::Base
       lifespan: "35-40 years",
       diet: "Herbivore",
       habitat: "Forest/Jungle",
-      image: "./images/gorilla.jpg",
+      image: "https://www.wwf.org.uk/sites/default/files/styles/social_share_image/public/2016-09/Medium_WW22557.jpg?itok=hg7S3zWD",
       alt_tag: "Picture of a Gorilla"
     },
     {
@@ -25,7 +25,7 @@ class PostController < Sinatra::Base
       lifespan: "9-10 years",
       diet: "Carnivore",
       habitat: "Caves",
-      image: "./images/vampire_bat.jpg",
+      image: "https://thumbs-prod.si-cdn.com/Rg0MNNgbSS5IdHvXFDZb9j9-mmo=/800x600/filters:no_upscale():focal(815x330:816x331)/https://public-media.si-cdn.com/filer/c4/5e/c45e215d-7646-4a2d-9586-611fa177e8c7/desmo-boden.jpg",
       alt_tag: "Picture of a Vampire Bat"
     },
     {
@@ -35,7 +35,7 @@ class PostController < Sinatra::Base
       lifespan: "10-12 years",
       diet: "Carnivore",
       habitat: "Tropical",
-      image: "./images/anaconda.jpg",
+      image: "https://media-cdn.tripadvisor.com/media/photo-s/0d/fc/47/63/anaconda.jpg",
       alt_tag: "Picture of an Anaconda"
     },
     {
@@ -45,7 +45,7 @@ class PostController < Sinatra::Base
       lifespan: "Infinity - we are all spider-pigs",
       diet: "The souls of the fallen",
       habitat: "Our hearts and minds",
-      image: "./images/spiderpig.jpeg",
+      image: "https://i.kym-cdn.com/photos/images/newsfeed/000/377/300/e30",
       alt_tag: "Picture of the Spider-Pig"
     },
     {
@@ -54,8 +54,8 @@ class PostController < Sinatra::Base
       species: "Marsupial",
       lifespan: "13-18 years",
       diet: "Herbivore",
-      habitat: "Forest",
-      image: "./images/koala.jpg",
+      habitat: "Forest", 
+      image: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA5OS83MTYvb3JpZ2luYWwva29hbGFzLXdpdGgtY2hsYW15ZGlhLmpwZw==",
       alt_tag: "Picture of a Koala"
     }]
 
